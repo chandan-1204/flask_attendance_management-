06b2a4df064041677cafc7d1b5e5757c86edf511
chandan-1204 <chandan.rakesh1204@gmail.com>
2026-04-22 15:36:47 +0530

    hello

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("home.html")
-
 
 @app.route("/about")
 def about():
@@ -19,6 +19,5 @@ def signup():
 def login():
     return render_template("login.html")
 
-if __name__ == "_main_":
-    app.run(debug=True,host="0.0.0.0",port=5000)
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
